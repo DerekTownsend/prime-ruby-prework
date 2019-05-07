831d171f8e5ac78448a90df440e5da037004bd92
Derek Townsend <derekjavon2@gmail.com>
2019-05-07 17:34:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,9 @@
 def prime?(num)
   flag=true
   if num<0
-    tempNum=num*-1
+    num*=-1
   end
-  (2..(tempNum-1)).each do |n|
+  (2..(num-1)).each do |n|
     if (num%n)==0
       flag=false
     end
